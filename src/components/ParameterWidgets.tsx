2f2ed62fe4f597a8dba0b73036eb71be5c49c810
digitalcluster25 <digitalcluster25@gmail.com>
2025-09-22 04:52:57 +0200

    Change widgets grid to 3 columns layout

--- src/components/ParameterWidgets.tsx
+++ src/components/ParameterWidgets.tsx
@@ -222,7 +222,7 @@ export default function ParameterWidgets() {
   }
 
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6">
       {parameters.map((parameter) => (
         <ParameterWidget key={parameter.id} parameter={parameter} />
       ))}
